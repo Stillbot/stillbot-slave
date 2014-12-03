7e86aae06fb2dfbcc0d2dd39230d7f39df277427
squamous <paul.mackin@gmail.com>
2014-12-03 22:51:35 +1100

    fixed K setters and refactored

--- src/sketch.ino
+++ src/sketch.ino
@@ -2,15 +2,11 @@
  * This file is basically the Arduino code from
  * github.com/thearn/Python-Arduino-Command-API plus the
  * OneWire/DallasTemperature features needed to take readings.
- *
- * TODO:
- *  - import PID controller code.
  */
 
 #include <SoftwareSerial.h>
 #include <Wire.h>
 #include <Servo.h>
-#include <EEPROM.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <PID_v1.h>
@@ -19,29 +15,37 @@
 
 #define DEFAULT_BAUDRATE 115200
 #define SSR_PIN 12
-#define DEFAULT_SETPOINT 78.37
-#define DEFAULT_WINDOW_SIZE 2000
+#define DUTY_CYCLE_SIZE 1000
+#define DEFAULT_SETPOINT 35.0
+//#define DEFAULT_SETPOINT 78.37
 #define DEFAULT_PID_THERMISTOR 0
 #define ONE_WIRE_BUS_PIN 10
 
-OneWire oneWire(ONE_WIRE_BUS_PIN);
+
+OneWire     oneWire(ONE_WIRE_BUS_PIN);
+
 DallasTemperature dt(&oneWire);
 
-double SetPoint = DEFAULT_SETPOINT, Input, Output = DEFAULT_WINDOW_SIZE;
-double agg_Kp = 4, agg_Ki=0.2, agg_Kd=1;           // Aggressive PID settings (< 10C from setpoint)
-double cons_Kp=1, cons_Ki=0.05, cons_Kd=0.25;  // Conservative PID settings
-PID pid(&Input, &Output, &SetPoint, agg_Kp, agg_Ki, agg_Kd, DIRECT);
+double      SetPoint = DEFAULT_SETPOINT,
+            Input,
+            Output;
+
+/* Aggressive PID settings (< 10C from setpoint) */
+double      Kp = 44,
+            Ki=164,
+            Kd=4;
+
+/* Conservative PID settings */
+double      cons_Kp=1,
+            cons_Ki=0.05,
+            cons_Kd=0.25;
+
+PID         pid(&Input, &Output, &SetPoint, Kp, Ki, Kd, DIRECT);
 unsigned int pid_thermistor_index = DEFAULT_PID_THERMISTOR;
-boolean boiler_on = false;
-boolean pid_on =false;
-int WindowSize = DEFAULT_WINDOW_SIZE;
+boolean     boiler_on = false;
+boolean     pid_on =false;
 unsigned long windowStartTime;
 
-
-SoftwareSerial *sserial = NULL;
-Servo servos[8];
-int servo_pins[] = {0, 0, 0, 0, 0, 0, 0, 0};
-boolean connected = false;
 
 int Str2int (String Str_value)
 {
@@ -76,71 +80,6 @@ float thermistorTemp(unsigned int thermistorIndex) {
     return dt.getTempCByIndex(thermistorIndex);
 }
 
-uint8_t readCapacitivePin(String data) {
-  int pinToMeasure = Str2int(data);
-  // readCapacitivePin
-  //  Input: Arduino pin number
-  //  Output: A number, from 0 to 17 expressing
-  //  how much capacitance is on the pin
-  //  When you touch the pin, or whatever you have
-  //  attached to it, the number will get higher
-  //  http://playground.arduino.cc/Code/CapacitiveSensor
-  //
-  // Variables used to translate from Arduino to AVR pin naming
-  volatile uint8_t* port;
-  volatile uint8_t* ddr;
-  volatile uint8_t* pin;
-  // Here we translate the input pin number from
-  //  Arduino pin number to the AVR PORT, PIN, DDR,
-  //  and which bit of those registers we care about.
-  byte bitmask;
-  port = portOutputRegister(digitalPinToPort(pinToMeasure));
-  ddr = portModeRegister(digitalPinToPort(pinToMeasure));
-  bitmask = digitalPinToBitMask(pinToMeasure);
-  pin = portInputRegister(digitalPinToPort(pinToMeasure));
-  // Discharge the pin first by setting it low and output
-  *port &= ~(bitmask);
-  *ddr  |= bitmask;
-  delay(1);
-  // Make the pin an input with the internal pull-up on
-  *ddr &= ~(bitmask);
-  *port |= bitmask;
-
-  // Now see how long the pin to get pulled up. This manual unrolling of the loop
-  // decreases the number of hardware cycles between each read of the pin,
-  // thus increasing sensitivity.
-  uint8_t cycles = 17;
-       if (*pin & bitmask) { cycles =  0;}
-  else if (*pin & bitmask) { cycles =  1;}
-  else if (*pin & bitmask) { cycles =  2;}
-  else if (*pin & bitmask) { cycles =  3;}
-  else if (*pin & bitmask) { cycles =  4;}
-  else if (*pin & bitmask) { cycles =  5;}
-  else if (*pin & bitmask) { cycles =  6;}
-  else if (*pin & bitmask) { cycles =  7;}
-  else if (*pin & bitmask) { cycles =  8;}
-  else if (*pin & bitmask) { cycles =  9;}
-  else if (*pin & bitmask) { cycles = 10;}
-  else if (*pin & bitmask) { cycles = 11;}
-  else if (*pin & bitmask) { cycles = 12;}
-  else if (*pin & bitmask) { cycles = 13;}
-  else if (*pin & bitmask) { cycles = 14;}
-  else if (*pin & bitmask) { cycles = 15;}
-  else if (*pin & bitmask) { cycles = 16;}
-
-  // Discharge the pin again by setting it low and output
-  //  It's important to leave the pins low if you want to 
-  //  be able to touch more than 1 sensor at a time - if
-  //  the sensor is left pulled high, when you touch
-  //  two sensors, your body will transfer the charge between
-  //  sensors.
-  *port &= ~(bitmask);
-  *ddr  |= bitmask;
-
-  //return cycles;
-  Serial.println(cycles);
-}
-
 void Tone(String data){
   int idx = data.indexOf('%');
   int len = Str2int(data.substring(0,idx));
@@ -165,203 +104,6 @@ void ToneNo(String data){
   int pin = Str2int(data);
   noTone(pin);
 } 
-
-void DigitalHandler(int mode, String data){
-      int pin = Str2int(data);
-    if(mode<=0){ //read
-        Serial.println(digitalRead(pin));
-    }else{
-        if(pin <0){
-            digitalWrite(-pin,LOW);
-        }else{
-            digitalWrite(pin,HIGH);
-        }
-        //Serial.println('0');
-    }
-}
-
-void AnalogHandler(int mode, String data){
-     if(mode<=0){ //read
-        int pin = Str2int(data);
-        Serial.println(analogRead(pin));
-    }else{
-        String sdata[2];
-        split(sdata,2,data,'%');
-        int pin = Str2int(sdata[0]);
-        int pv = Str2int(sdata[1]);
-        analogWrite(pin,pv);
-    }
-}
-
-void ConfigurePinHandler(String data){
-    int pin = Str2int(data);
-    if(pin <=0){
-        pinMode(-pin,INPUT);
-    }else{
-        pinMode(pin,OUTPUT);
-    }
-}
-
-void shiftOutHandler(String data) {    
-    String sdata[4];
-    split(sdata, 4, data, '%');
-    int dataPin = sdata[0].toInt();
-    int clockPin = sdata[1].toInt();
-    String bitOrderName = sdata[2];
-    byte value = (byte)(sdata[3].toInt());
-    if (bitOrderName == "MSBFIRST") {
-       shiftOut(dataPin, clockPin, MSBFIRST, value);
-    } else {
-       shiftOut(dataPin, clockPin, LSBFIRST, value);
-    }
-}
-
-void shiftInHandler(String data) {
-    String sdata[3];
-    split(sdata, 3, data, '%');
-    int dataPin = sdata[0].toInt();
-    int clockPin = sdata[1].toInt();
-    String bitOrderName = sdata[2];
-    int incoming;
-    if (bitOrderName == "MSBFIRST") {
-       incoming = (int)shiftIn(dataPin, clockPin, MSBFIRST);
-    } else {
-       incoming = (int)shiftIn(dataPin, clockPin, LSBFIRST);
-    }
-    Serial.println(incoming);
-}
-
-void SS_set(String data){
-  delete sserial;
-  String sdata[3];
-  split(sdata,3,data,'%');
-  int rx_ = Str2int(sdata[0]);
-  int tx_ = Str2int(sdata[1]);
-  int baud_ = Str2int(sdata[2]);
-  sserial = new SoftwareSerial(rx_, tx_);
-  sserial->begin(baud_);
-  Serial.println("ss OK");
-}
-
-void SS_write(String data) {
- int len = data.length()+1;
- char buffer[len];
- data.toCharArray(buffer,len);
- Serial.println("ss OK");
- sserial->write(buffer); 
-}
-void SS_read(String data) {
- char c = sserial->read(); 
- Serial.println(c);
-}
-
-void pulseInHandler(String data){
-    int pin = Str2int(data);
-    long duration;
-    if(pin <=0){
-          pinMode(-pin, INPUT);
-          duration = pulseIn(-pin, LOW);      
-    }else{
-          pinMode(pin, INPUT);
-          duration = pulseIn(pin, HIGH);      
-    }
-    Serial.println(duration);
-}
-
-void pulseInSHandler(String data){
-    int pin = Str2int(data);
-    long duration;
-    if(pin <=0){
-          pinMode(-pin, OUTPUT);
-          digitalWrite(-pin, HIGH);
-          delayMicroseconds(2);
-          digitalWrite(-pin, LOW);
-          delayMicroseconds(5);
-          digitalWrite(-pin, HIGH);
-          pinMode(-pin, INPUT);
-          duration = pulseIn(-pin, LOW);      
-    }else{
-          pinMode(pin, OUTPUT);
-          digitalWrite(pin, LOW);
-          delayMicroseconds(2);
-          digitalWrite(pin, HIGH);
-          delayMicroseconds(5);
-          digitalWrite(pin, LOW);
-          pinMode(pin, INPUT);
-          duration = pulseIn(pin, HIGH);      
-    }
-    Serial.println(duration);
-}
-
-void SV_add(String data) {
-    String sdata[3];
-    split(sdata,3,data,'%');
-    int pin = Str2int(sdata[0]);
-    int min = Str2int(sdata[1]);
-    int max = Str2int(sdata[2]);
-    int pos = -1;
-    for (int i = 0; i<8;i++) {
-        if (servo_pins[i] == pin) { //reset in place
-            servos[pos].detach();
-            servos[pos].attach(pin, min, max);
-            servo_pins[pos] = pin;
-            Serial.println(pos);
-            return;
-            }
-        }
-    for (int i = 0; i<8;i++) {
-        if (servo_pins[i] == 0) {pos = i;break;} // find spot in servo array
-        }
-    if (pos == -1) {;} //no array position available!
-    else {
-        servos[pos].attach(pin, min, max);
-        servo_pins[pos] = pin;
-        Serial.println(pos);
-        }
-}
-
-void SV_remove(String data) {
-    int pos = Str2int(data);
-    servos[pos].detach();
-    servo_pins[pos] = 0;
-}
-
-void SV_read(String data) {
-    int pos = Str2int(data);
-    int angle;
-    angle = servos[pos].read();
-    Serial.println(angle);
-}
-
-void SV_write(String data) {
-    String sdata[2];
-    split(sdata,2,data,'%');
-    int pos = Str2int(sdata[0]);
-    int angle = Str2int(sdata[1]);
-    servos[pos].write(angle);
-}
-
-void SV_write_ms(String data) {
-    String sdata[2];
-    split(sdata,2,data,'%');
-    int pos = Str2int(sdata[0]);
-    int uS = Str2int(sdata[1]);
-    servos[pos].writeMicroseconds(uS);
-}
-
-void sizeEEPROM() {
-    Serial.println(E2END + 1);
-}
-
-void EEPROMHandler(int mode, String data) {
-    String sdata[2];
-    split(sdata, 2, data, '%');
-    if (mode == 0) {  
-        EEPROM.write(Str2int(sdata[0]), Str2int(sdata[1]));  
-    } else {
-        Serial.println(EEPROM.read(Str2int(sdata[0])));
-    }
-}
 
 void SerialParser(void) {
   char readChar[64];
@@ -388,78 +130,9 @@ void SerialParser(void) {
   String data = read_.substring(idx1+1,idx2);
   
   // determine command sent
-  if (cmd == "dw") {
-      DigitalHandler(1, data);   
-  }
-  else if (cmd == "dr") {
-      DigitalHandler(0, data);   
-  }  
-  else if (cmd == "aw") {
-      AnalogHandler(1, data);   
-  }    
-  else if (cmd == "ar") {
-      AnalogHandler(0, data);   
-  }      
-  else if (cmd == "pm") {
-      ConfigurePinHandler(data);   
-  }    
-  else if (cmd == "ps") {
-      pulseInSHandler(data);   
-  }    
-  else if (cmd == "pi") {
-      pulseInHandler(data);   
-  }        
-  else if (cmd == "ss") {
-      SS_set(data);   
-  }
-  else if (cmd == "sw") {
-      SS_write(data);   
-  }
-  else if (cmd == "sr") {
-      SS_read(data);   
-  }    
-  else if (cmd == "sva") {
-      SV_add(data);   
-  }      
-  else if (cmd == "svr") {
-      SV_read(data);   
-  }   
- else if (cmd == "svw") {
-      SV_write(data);   
-  }    
- else if (cmd == "svwm") {
-      SV_write_ms(data);   
-  }      
-  else if (cmd == "svd") {
-      SV_remove(data);   
-  } 
-  else if (cmd == "version") {
+  if (cmd == "version") {
       Version();   
   }
-  else if (cmd == "to") {
-      Tone(data);   
-  } 
-  else if (cmd == "nto") {
-      ToneNo(data);   
-  }  
-  else if (cmd == "cap") {
-      readCapacitivePin(data);   
-  }
-  else if (cmd == "so") {
-      shiftOutHandler(data);
-  }
-  else if (cmd == "si") {
-      shiftInHandler(data);
-  }
-  else if (cmd == "eewr") {
-      EEPROMHandler(0, data);   
-  } 
-  else if (cmd == "eer") {
-      EEPROMHandler(1, data);   
-  }  
-  else if (cmd == "sz") {  
-      sizeEEPROM();
-  }  
   else if (cmd == "dscount") {
       Serial.println(dt.getDeviceCount());
   }
@@ -487,14 +160,11 @@ void SerialParser(void) {
         Serial.println("NO_THERMISTORS_PRESENT");
         return;
       }
-//      pid.SetMode(AUTOMATIC);
       pid_on = true;
     } else if (data == "off") {
-//      pid.SetMode(MANUAL);
       pid_on = false;
     }
     if (pid_on == true) {
-//    if (pid.GetMode() == AUTOMATIC) {
         Serial.println("on");
     } else {
         Serial.println("off");
@@ -504,29 +174,38 @@ void SerialParser(void) {
     if (data == "") {
       Serial.println(SetPoint);
     } else {
-      int new_sp = Str2int(data);
-      SetPoint = new_sp;
+      SetPoint = Str2int(data);
       Serial.println(SetPoint);
     }
   }
   else if (cmd == "kp") {
-    if (data != "") {
+    if (data == "") {
+      Serial.println(pid.GetKp());
+    } else {
       pid.SetTunings(Str2float(data), pid.GetKi(), pid.GetKd());
+      Serial.println(pid.GetKp());
     }
-    Serial.println(pid.GetKp());
   }
   else if (cmd == "ki") {
-    if (data != "") {
+    if (data == "") {
+      Serial.println(pid.GetKi());
+    } else {
       pid.SetTunings(pid.GetKp(), Str2float(data), pid.GetKd());
+      Serial.println(pid.GetKi());
     }
-    Serial.println(pid.GetKi());
   }
   else if (cmd == "kd") {
-    if (data != "") {
-      pid.SetTunings(pid.GetKp(), pid.GetKp(), Str2float(data));
+    if (data == "") {
+      Serial.println(pid.GetKd());
+    } else {
+      pid.SetTunings(pid.GetKp(), pid.GetKi(), Str2float(data));
+      Serial.println(pid.GetKd());
     }
-    Serial.println(pid.GetKd());
   }
+  else if (cmd == "output") {
+    Serial.println(Output);
+  }
+  
 }
 
 void setup()  {
@@ -543,26 +222,23 @@ void setup()  {
   
   pinMode(SSR_PIN, OUTPUT); 
   
-  windowStartTime = millis();
-  pid.SetOutputLimits(0, WindowSize);  // Tell the PID code to produce an output value in the range 0 - WindowSize
+  pid.SetTunings(Kp, Ki, Kd);
+  pid.SetOutputLimits(0, DUTY_CYCLE_SIZE);          // PID output range: 0 - DUTY_CYCLE_SIZE
   pid.SetMode(AUTOMATIC);
+  
+  windowStartTime = millis();
 }
 
 void loop() {
    unsigned long now = millis();
-   Input = thermistorTemp(pid_thermistor_index);
-   double gap = abs(SetPoint - Input); // distance away from setpoint
-   
-   if (gap < 10) {
-     pid.SetTunings(cons_Kp, cons_Ki, cons_Kd);
-   } else {
-     pid.SetTunings(agg_Kp, agg_Ki, agg_Kd);
-   }
-   pid.Compute();
-   
 
-   if (now - windowStartTime > WindowSize) {
-     windowStartTime += WindowSize;
+   Input = thermistorTemp(pid_thermistor_index);
+   double gap = abs(SetPoint - Input);              // distance away from setpoint
+   
+   pid.Compute();
+
+   if (now - windowStartTime > DUTY_CYCLE_SIZE) {
+     windowStartTime += DUTY_CYCLE_SIZE;
    }
    
    if (pid_on) {
@@ -573,7 +249,7 @@ void loop() {
        digitalWrite(SSR_PIN, LOW);
        boiler_on = false;
      }
-  }
-  
+   }
+
   SerialParser();
 }
